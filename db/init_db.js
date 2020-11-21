bc1a4c08535ec98195e15c268550c826e093e935
techniguy <youknowgd@gmail.com>
2020-11-21 09:09:22 -0800

    getting the latest info

--- db/init_db.js
+++ db/init_db.js
@@ -7,11 +7,28 @@ const {
 async function buildTables() {
   try {
     client.connect();
-
+     try {
+       client.query(`
+       DROP TABLE IF EXISTS activities;
+       `)
+     } catch (error) {
+       
+     }
     // drop tables in correct order
 
     // build tables in correct order
 
+    try {
+      client.query(`
+      
+      CREATE TABLE activities(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) UNIQUE NOT NULL,
+        description TEXT NOT NULL
+      );`)
+    } catch (error) {
+      
+    }
   } catch (error) {
     throw error;
   }
@@ -20,6 +37,11 @@ async function buildTables() {
 async function populateInitialData() {
   try {
     // create useful starting data
+    client.connect();
+    // await dropTables();
+    // await createTables();
+    // await createInitialUsers();
+    
   } catch (error) {
     throw error;
   }
