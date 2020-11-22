e2ef7378e14ef21e3ca60a37be6d2be732dcf766
Riley Smith <rileyshelsmith@gmail.com>
2020-11-22 14:28:28 -0800

    fixed error from merge conflict

--- db/init_db.js
+++ db/init_db.js
@@ -1,7 +1,5 @@
 const { client } = require('./index');
 const { createOrder } = require('./orders');
-const { client } = require('./index');
-const { getAllOrders, createOrder } = require('./orders');
 
 const { createUser } = require('./users');
 
