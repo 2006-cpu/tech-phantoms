fe15148d9c78152bef5ac51df60bfbb94a7a5992
Tammy Allen <tjgallen@aol.com>
2020-11-29 11:42:52 -0800

    AddProductToOrder DB Adapter #68

--- db/init_db.js
+++ db/init_db.js
@@ -5,8 +5,6 @@ const { createUser } = require('./users');
 
 const { createProduct } = require('./products');
 
-const { destroyOrderProduct } = require('./order_products');
-
 async function buildTables() {
   try {
     client.connect();
--- db/order_products.js
+++ db/order_products.js
@@ -1,5 +1,23 @@
 const { client } = require('./index');
 
+async function addProductToOrder({
+    orderId,
+    productId,
+    price,
+    quantity
+}) {
+try {
+    // if the productId is NOT on the order yet, create a new order_products
+    const { rows: [productOrder] } = await client.query(`
+    INSERT INTO product_orders ("productId", "orderId", price, quantity)
+    VALUES($1, $2, $3, $4)
+    RETURNING *
+    `, [productId, orderId, price, quantity])
+    return productOrder;
+    } catch (error) {
+    }
+};
+
 async function destroyOrderProduct(id) {
     try {
       const { rows: [orderProduct] } = await client.query(`
@@ -14,5 +32,6 @@ async function destroyOrderProduct(id) {
 };
 
 module.exports={
+    addProductToOrder,
     destroyOrderProduct
 }
