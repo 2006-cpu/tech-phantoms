1f655295cb41c12e6f57f069ea01441371a0d6a8
techniguy <youknowgd@gmail.com>
2020-11-28 23:03:28 -0800

    added getOrderProductByIdNUpdateOrderProduct

--- db/order_products.js
+++ db/order_products.js
@@ -1,5 +1,36 @@
 const { client } = require('./index');
 
+
+async function getOrderProductById(id) {
+    try {
+      const { rows: [orderProduct] } = await client.query(`
+      SELECT * 
+      FROM order_products
+      WHERE id=$1 
+      `, [id]);
+      return orderProduct;
+    } catch (error) {
+        throw error;
+    }
+};
+
+// ***********************************************************************************
+
+async function updateOrderProduct({ id, price, quantity }) {
+    try {
+      const { rows: [orderProduct] } = await client.query(`
+      UPDATE order_products op
+      SET price=$2, quantity=$3
+      WHERE op.id=$1;
+      `, [id,price,quantity]);
+      return orderProduct;
+    } catch (error) {
+        throw error;
+    }
+};
+
+// ***********************************************************************************
+
 async function destroyOrderProduct(id) {
     try {
       const { rows: [orderProduct] } = await client.query(`
@@ -12,7 +43,8 @@ async function destroyOrderProduct(id) {
         throw error;
     }
 };
-
 module.exports={
+    getOrderProductById,
+    updateOrderProduct,
     destroyOrderProduct
 }
