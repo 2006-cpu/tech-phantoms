966d66cc37caa381533103f568bd1ece21bd9b3f
techniguy <youknowgd@gmail.com>
2020-12-06 23:35:18 -0800

    saving current info

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,12 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
-import {getOrdersCart, removeProductFromCart} from '../api';
+import {getOrdersCart} from '../api';
 import './Cart.css';
-<<<<<<< HEAD
 import Stripecc from './Stripe';
-=======
-import axios from 'axios'
->>>>>>> dev
 
 const Cart = (props) => {
     const {token, user} = props
@@ -16,7 +12,7 @@ const Cart = (props) => {
     const {username} = user
 
     useEffect(() => {
-        getOrdersCart(token).then( cart=> {
+        getOrdersCart(token).then(cart=> {
         const cartProductData = cart.orderProducts.map((orderProduct)=>{           
             const product = () =>{ for(let i=0; i<cart.products.length; i++){
                 if(cart.products[i].id===orderProduct.productId){
@@ -24,14 +20,9 @@ const Cart = (props) => {
                     return {product, price: orderProduct.price, quantity: orderProduct.quantity}
                 }
             }}
-           
             return product()
         })
-        const cartTotalData= {
-            id:cart.id,
-            products: cartProductData
-        }
-        setCart(cartTotalData)
+        setCart(cartProductData)
             let total = 0
             for(let i=0;i<cartProductData.length;i++){
                 total=total+cartProductData[i].price
@@ -46,22 +37,19 @@ return <>
     <div className="cartDiv">
         <div className="cartItems">
         <h2 className="userId">{username}'s Cart</h2>
-        {   cart.products ?
-            cart.products.map((cartProduct)=>{
+        {
+            cart.map((cartProduct)=>{
                 const {product, price, quantity} = cartProduct
                 return<div key={'cartProduct'+product.id} className = 'cartProduct'>
                     <img src={product.imageURL} className='productImage'></img>
                     <h3>{product.name}</h3>
                     <span>Quantity:{quantity}</span>
                     <span>Price: {price}</span>
-                    <button onClick={()=>{removeProductFromCart(cart.id, product.id, token).then((removed)=>{setUpdateCart('Removed'+removed.name)})}}>Remove</button>
                 </div>
             })
-            :
-            <div>Cart is Empty!</div>
         }
         <div>Total Price: {totalPrice}</div>
-        <Stripecc>Pay Order</Stripecc>
+        <Stripecc >Pay Order</Stripecc>
 
         </div>
     </div>
