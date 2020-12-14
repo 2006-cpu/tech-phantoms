beb360df08c3f206a33bb0c93e63e76d3854d7b7
techniguy <youknowgd@gmail.com>
2020-12-13 18:04:14 -0800

    added a few things

--- src/components/Stripe.js
+++ src/components/Stripe.js
@@ -2,9 +2,7 @@ import React from 'react'
 import axios from 'axios';
 import StripeCheckout from 'react-stripe-checkout';
 import { useHistory } from 'react-router-dom'
-import swal from 'sweetalert';
 
- 
 const PKSECRET = process.env.NODE_ENV === 'production'
   ? 'pk_test_51Ht0x3E3GFmNRfP2y3zQEwL39Agff3XwI80izmf3M8Uy4LNsLTFQf04FDw6dwomXIp9RqHG9slWonTc5xJIO5jQp001IQpFHC6'
   : 'pk_test_51Ht0x3E3GFmNRfP2y3zQEwL39Agff3XwI80izmf3M8Uy4LNsLTFQf04FDw6dwomXIp9RqHG9slWonTc5xJIO5jQp001IQpFHC6';
@@ -17,29 +15,16 @@ const CURRENCY = 'USD';
 
 const centsToDollar = amount => amount * 100;
  
- /*
-const onToken = (amount, description) => token =>{
-  axios.post(PAYMENT_SERVER_URL,
-    {
-      description,
-      source: token.id,
-      currency: CURRENCY,
-      amount: centsToDollar(amount)
-    })
-    .then(successPayment)
-    .catch(errorPayment);}
- */
 const Stripecc = ({ description, amount }) =>{
 
   const history = useHistory();
 
   const successPayment = () => {  
-  swal("Order Submitted. Thank you for shopping at Dope Soap!","success")
   history.push('/Success')
   };
 
-const errorPayment = data => {
-};
+  const errorPayment = () => {
+  };
 
     const onToken = (amount, description) => token =>{
       axios.post(PAYMENT_SERVER_URL,
--- src/components/Success.js
+++ src/components/Success.js
@@ -1,5 +1,7 @@
 import React, {useEffect} from 'react'
 import { completeOrder, getOrdersCart } from '../api';
+import './Success.css';
+import bubble02 from './images/bubble02.png';
 
 const Success = (props) =>{
     const {token} = props
@@ -9,7 +11,28 @@ const Success = (props) =>{
         console.log('SUCCESS', cart)
     },[])
     
-    return <h1>Thank you for your Order!</h1>
+    return <div>
+    <div className="welcomeDiv">
+
+        <div className="bubbleSetOneOne">
+            <img src={bubble02} className="bubbleOneOne" alt="bubbleOne" width="100px" height="100px" />
+            <img src={bubble02} className="bubbleTwoTwo" alt="bubbleTwo" width="150px" height="150px" />
+        </div> 
+
+        <div><br /><br/>
+        <h1>Thank you for your Order!</h1></div>
+
+        <div className="bubbleSetTwoTwo">
+            <img src={bubble02} className="bubbleThreeThree" alt="bubbleThree" width="300px" height="300px" />
+        </div> 
+
+    </div>
+
+</div>
+    
+    
+    
+    // <h1 className="success">Thank you for your Order!</h1>
 }
 
 export default Success
